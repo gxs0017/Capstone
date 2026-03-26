fb2c0f07b47a074b172658ecd631be9855be360e
Alisson Johnson <joalisso@sheridancollege.ca>
2026-03-26 14:39:53 -0400

    Updated backend connections

---
+++ backend/controllers/Controller.js
@@ -0,0 +1,87 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const registerUser = async (req, res) => {
+    try {
+        const { firstName, lastName, dateOfBirth, email, password, phoneNumber, address, role, services } = req.body; 
+        
+        const existingUser = await User.findOne({ email: email });
+        
+        if (existingUser) {
+            return res.status(400).json({ message: "User with this email already exists." });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            dateOfBirth,
+            email,
+            password: hashedPassword,
+            phoneNumber,
+            address,
+            role,
+            services
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            token: 'dummy-token',
+            user: {
+                id: newUser._id,
+                email: newUser.email,
+                role: newUser.role,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName
+            }
+        });
+
+    } catch (error) {
+        console.error("Registration Error:", error);
+        res.status(500).json({ message: "Server error during registration", error: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        res.json({
+            token: 'dummy-token',
+            user: {
+                id: user.id,
+                email: user.email,
+                role: user.role,
+                firstName: user.firstName,
+                lastName: user.lastName
+            }
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+};
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);       
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching data", error: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser, getUsers };
---
+++ frontend/.angular/cache/21.2.2/Neighbourhood-Booking-App/vite/deps/chunk-ACK64OIO.js
@@ -0,0 +1,120 @@
+import {
+  isPlatformBrowser
+} from "./chunk-KR57MFJR.js";
+import {
+  ApplicationRef,
+  ElementRef,
+  EnvironmentInjector,
+  Injectable,
+  Injector,
+  PLATFORM_ID,
+  createComponent,
+  inject,
+  setClassMetadata,
+  ɵɵdefineInjectable
+} from "./chunk-G452VKMN.js";
+
+// node_modules/@angular/cdk/fesm2022/_platform-chunk.mjs
+var hasV8BreakIterator;
+try {
+  hasV8BreakIterator = typeof Intl !== "undefined" && Intl.v8BreakIterator;
+} catch {
+  hasV8BreakIterator = false;
+}
+var Platform = class _Platform {
+  _platformId = inject(PLATFORM_ID);
+  isBrowser = this._platformId ? isPlatformBrowser(this._platformId) : typeof document === "object" && !!document;
+  EDGE = this.isBrowser && /(edge)/i.test(navigator.userAgent);
+  TRIDENT = this.isBrowser && /(msie|trident)/i.test(navigator.userAgent);
+  BLINK = this.isBrowser && !!(window.chrome || hasV8BreakIterator) && typeof CSS !== "undefined" && !this.EDGE && !this.TRIDENT;
+  WEBKIT = this.isBrowser && /AppleWebKit/i.test(navigator.userAgent) && !this.BLINK && !this.EDGE && !this.TRIDENT;
+  IOS = this.isBrowser && /iPad|iPhone|iPod/.test(navigator.userAgent) && !("MSStream" in window);
+  FIREFOX = this.isBrowser && /(firefox|minefield)/i.test(navigator.userAgent);
+  ANDROID = this.isBrowser && /android/i.test(navigator.userAgent) && !this.TRIDENT;
+  SAFARI = this.isBrowser && /safari/i.test(navigator.userAgent) && this.WEBKIT;
+  constructor() {
+  }
+  static ɵfac = function Platform_Factory(__ngFactoryType__) {
+    return new (__ngFactoryType__ || _Platform)();
+  };
+  static ɵprov = ɵɵdefineInjectable({
+    token: _Platform,
+    factory: _Platform.ɵfac,
+    providedIn: "root"
+  });
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(Platform, [{
+    type: Injectable,
+    args: [{
+      providedIn: "root"
+    }]
+  }], () => [], null);
+})();
+
+// node_modules/@angular/cdk/fesm2022/_style-loader-chunk.mjs
+var appsWithLoaders = /* @__PURE__ */ new WeakMap();
+var _CdkPrivateStyleLoader = class __CdkPrivateStyleLoader {
+  _appRef;
+  _injector = inject(Injector);
+  _environmentInjector = inject(EnvironmentInjector);
+  load(loader) {
+    const appRef = this._appRef = this._appRef || this._injector.get(ApplicationRef);
+    let data = appsWithLoaders.get(appRef);
+    if (!data) {
+      data = {
+        loaders: /* @__PURE__ */ new Set(),
+        refs: []
+      };
+      appsWithLoaders.set(appRef, data);
+      appRef.onDestroy(() => {
+        appsWithLoaders.get(appRef)?.refs.forEach((ref) => ref.destroy());
+        appsWithLoaders.delete(appRef);
+      });
+    }
+    if (!data.loaders.has(loader)) {
+      data.loaders.add(loader);
+      data.refs.push(createComponent(loader, {
+        environmentInjector: this._environmentInjector
+      }));
+    }
+  }
+  static ɵfac = function _CdkPrivateStyleLoader_Factory(__ngFactoryType__) {
+    return new (__ngFactoryType__ || __CdkPrivateStyleLoader)();
+  };
+  static ɵprov = ɵɵdefineInjectable({
+    token: __CdkPrivateStyleLoader,
+    factory: __CdkPrivateStyleLoader.ɵfac,
+    providedIn: "root"
+  });
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(_CdkPrivateStyleLoader, [{
+    type: Injectable,
+    args: [{
+      providedIn: "root"
+    }]
+  }], null, null);
+})();
+
+// node_modules/@angular/cdk/fesm2022/_element-chunk.mjs
+function coerceNumberProperty(value, fallbackValue = 0) {
+  if (_isNumberValue(value)) {
+    return Number(value);
+  }
+  return arguments.length === 2 ? fallbackValue : 0;
+}
+function _isNumberValue(value) {
+  return !isNaN(parseFloat(value)) && !isNaN(Number(value));
+}
+function coerceElement(elementOrRef) {
+  return elementOrRef instanceof ElementRef ? elementOrRef.nativeElement : elementOrRef;
+}
+
+export {
+  Platform,
+  _CdkPrivateStyleLoader,
+  coerceNumberProperty,
+  coerceElement
+};
+//# sourceMappingURL=chunk-ACK64OIO.js.map
